a18afdc19490c83e5bc6794a1dbc355816cc8d20
copaiement <cole.paiement@gmail.com>
2023-03-01 20:54:40 -0800

    Final updates. Added JS script to validate PW

--- client-validation.js
+++ client-validation.js
@@ -1,1 +1,23 @@
 // client-side password validation
+
+function verifyPwd() {
+    let pwd = document.querySelector("#pwd").value;
+    let pwdConfirm = document.querySelector("#pwd-confirm").value;
+    const pwdRejectMsg = document.querySelector(".pwd-reject");
+
+    if (pwd == "" || pwdConfirm == "") {
+        pwdRejectMsg.textContent = "**Please enter a password";
+        return false;
+    } else if (pwd.length < 8) {
+        pwdRejectMsg.textContent = "**Password is too short";
+        return false;
+    } else if (!/^[a-z\d]{8,}$/i.test(pwd)) {
+        pwdRejectMsg.textContent = "**Please use 8 or more letters and numbers only"
+        return false;
+    } else if (pwd !== pwdConfirm) {
+        pwdRejectMsg.textContent = "**Passwords do not match";
+        return false;
+    } else {
+        return true;
+    }
+}
